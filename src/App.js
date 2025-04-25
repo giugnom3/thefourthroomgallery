ac351f46e752aac391c27fff54fe91e35bacc943
Maria Giugno <giugnom3@southernct.edu>
2025-04-25 19:56:04 -0400

    upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GalleryScene from './virtual-gallery/gallery';
+
+function App() {
+  return (
+    <div className="App">
+      <GalleryScene />
+    </div>
+  );
+}
+
+export default App;
