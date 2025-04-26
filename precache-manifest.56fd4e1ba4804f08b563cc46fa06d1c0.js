a1d0da27b1b1edf486e3b79a85ff1d0fd448e508
Maria Giugno <giugnom3@southernct.edu>
2025-04-25 20:23:20 -0400

    Updates

---
+++ precache-manifest.56fd4e1ba4804f08b563cc46fa06d1c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4666b81b4973b81dc32be7c4c76e9fed",
+    "url": "/thefourthroomgallery/index.html"
+  },
+  {
+    "revision": "7533179f256cf329f64b",
+    "url": "/thefourthroomgallery/static/css/main.06ccb946.chunk.css"
+  },
+  {
+    "revision": "f327438644980bc1e5e4",
+    "url": "/thefourthroomgallery/static/js/2.f3c19b94.chunk.js"
+  },
+  {
+    "revision": "7533179f256cf329f64b",
+    "url": "/thefourthroomgallery/static/js/main.84895fe8.chunk.js"
+  },
+  {
+    "revision": "eac58b71248bfa311fe2",
+    "url": "/thefourthroomgallery/static/js/runtime~main.feea192a.js"
+  }
+]);
