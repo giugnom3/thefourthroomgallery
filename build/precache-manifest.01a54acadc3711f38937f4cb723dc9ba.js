b46e25a51affff5752fcc78f723b874eef443637
Maria Giugno <giugnom3@southernct.edu>
2025-04-30 21:02:41 -0400

    updates

---
+++ build/precache-manifest.01a54acadc3711f38937f4cb723dc9ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40815667020ff6bc19cd510abd8e54d6",
+    "url": "/thefourthroomgallery/index.html"
+  },
+  {
+    "revision": "2d201bd2209ad1cc0a5f",
+    "url": "/thefourthroomgallery/static/css/main.06ccb946.chunk.css"
+  },
+  {
+    "revision": "a738c41ebf2357b5b8e7",
+    "url": "/thefourthroomgallery/static/js/2.1d1eaa3f.chunk.js"
+  },
+  {
+    "revision": "2d201bd2209ad1cc0a5f",
+    "url": "/thefourthroomgallery/static/js/main.105b0a7e.chunk.js"
+  },
+  {
+    "revision": "eac58b71248bfa311fe2",
+    "url": "/thefourthroomgallery/static/js/runtime~main.feea192a.js"
+  }
+]);
---
+++ build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/thefourthroomgallery/precache-manifest.01a54acadc3711f38937f4cb723dc9ba.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thefourthroomgallery/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
